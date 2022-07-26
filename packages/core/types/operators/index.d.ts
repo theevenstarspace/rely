eb92048d09e28a602416be8d684b19331f038445
Sergey Sharpov <sharpov.sergey@theevenstar.net>
2022-07-26 12:09:56 +0300

    cache clean

--- packages/core/types/operators/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ITagData } from "../tag/types";
-export declare function attrOp<DataType extends ITagData>(): import("rxjs").OperatorFunction<DataType, DataType["attrs"]>;
-export declare function childOp<DataType extends ITagData>(): import("rxjs").OperatorFunction<DataType, DataType["children"]>;
